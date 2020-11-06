509f45d924006d665ca9b797a87b247380491c72
Pablo <pablopezgarcia97@gmail.com>
2020-11-06 11:17:34 +0100

    añadir password username y button

---
+++ src/main/java/com/dam/practicandointerfacesgraficas/ConstrainingActivity.java
@@ -0,0 +1,14 @@
+package com.dam.practicandointerfacesgraficas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraining);
+    }
+}
